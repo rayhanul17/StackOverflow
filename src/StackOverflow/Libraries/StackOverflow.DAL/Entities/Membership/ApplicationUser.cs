2806eb623a0b72ff7734f0478addd5ed58b9c335
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-04-25 13:33:13 +0600

    Login Done

--- src/StackOverflow/Libraries/StackOverflow.DAL/Entities/Membership/ApplicationUser.cs
+++ src/StackOverflow/Libraries/StackOverflow.DAL/Entities/Membership/ApplicationUser.cs
@@ -5,5 +5,5 @@ namespace StackOverflow.DAL.Entities.Membership;
 public class ApplicationUser : IdentityUser<Guid>
 {
     public virtual string FirstName { get; set; } = null!;
-    public virtual string LastName { get; set; } = null!;   
+    public virtual string LastName { get; set; } = null!; 
 }
--- src/StackOverflow/Libraries/StackOverflow.DAL/Utility/MsSqlSessionFactory.cs
+++ src/StackOverflow/Libraries/StackOverflow.DAL/Utility/MsSqlSessionFactory.cs
@@ -2,6 +2,7 @@
 using FluentNHibernate.Cfg.Db;
 using NHibernate;
 using NHibernate.Tool.hbm2ddl;
+using StackOverflow.DAL.Mappings;
 
 namespace StackOverflow.DAL.Utility;
 
@@ -14,7 +15,7 @@ public class MsSqlSessionFactory : IDataSessionFactory
         Session = Fluently
             .Configure()
             .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionString))
-            .Mappings(m => m.FluentMappings.AddFromAssemblyOf<MsSqlSessionFactory>())
+            .Mappings(m => m.FluentMappings.AddFromAssemblyOf<MappingReference>())
             .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
             .BuildSessionFactory();
     }
