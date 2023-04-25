f134cd039b2848dce3a0feea69a42e0748c44430
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-04-25 13:40:43 +0600

    Login Registration Done

--- src/StackOverflow/Libraries/StackOverflow.DAL/Utility/MsSqlSessionFactory.cs
+++ src/StackOverflow/Libraries/StackOverflow.DAL/Utility/MsSqlSessionFactory.cs
@@ -15,7 +15,7 @@ public class MsSqlSessionFactory : IDataSessionFactory
         Session = Fluently
             .Configure()
             .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionString))
-            .Mappings(m => m.FluentMappings.AddFromAssemblyOf<MappingReference>())
+            .Mappings(m => m.FluentMappings.AddFromAssemblyOf<MsSqlSessionFactory>())
             .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
             .BuildSessionFactory();
     }
